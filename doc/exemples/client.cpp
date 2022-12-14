aae69a1bf285a93964572d2c5e61a2df8c6a2b0b
Daniel Thalmann <daniel@thalmann.li>
2022-12-14 21:06:45 +0100

    exemple server

--- doc/exemples/client.cpp
+++ doc/exemples/client.cpp
@@ -6,42 +6,42 @@
  
 int main(){
  
-  const char *ip = "127.0.0.1";
-  int port = 2042;
- 
-  int sock;
-  struct sockaddr_in addr;
-  
-  char buffer[1024];
- 
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0){
-    perror("[-]Socket error");
-    exit(1);
-  }
-  printf("[+]TCP server socket created.\n");
- 
-  memset(&addr, '\0', sizeof(addr));
-  addr.sin_family = AF_INET;
-  addr.sin_port = port;
-  addr.sin_addr.s_addr = inet_addr(ip);
- 
-  connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-  printf("Connected to the server.\n");
- 
- while (1)
-{
-  bzero(buffer, 1024);
-  strcpy(buffer, "HELLO, THIS IS CLIENT.");
-  printf("Client: %s\n", buffer);
-  send(sock, buffer, strlen(buffer), 0);
- 
-  bzero(buffer, 1024);
-  recv(sock, buffer, sizeof(buffer), 0);
-  printf("Server: %s\n", buffer);
-}
-  close(sock);
-  printf("Disconnected from the server.\n");
- 
-  return 0;
+	const char *ip = "127.0.0.1";
+	int port = 2042;
+
+	int sock;
+	struct sockaddr_in addr;
+
+	char buffer[1024];
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0){
+		perror("[-]Socket error");
+		exit(1);
+	}
+	printf("[+]TCP server socket created.\n");
+
+	memset(&addr, '\0', sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = port;
+	addr.sin_addr.s_addr = inet_addr(ip);
+
+	connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+	printf("Connected to the server.\n");
+
+	while (1)
+	{
+		bzero(buffer, 1024);
+		strcpy(buffer, "HELLO, THIS IS CLIENT.");
+		printf("Client: %s\n", buffer);
+		send(sock, buffer, strlen(buffer), 0);
+		
+		bzero(buffer, 1024);
+		recv(sock, buffer, sizeof(buffer), 0);
+		printf("Server: %s\n", buffer);
+	}
+	close(sock);
+	printf("Disconnected from the server.\n");
+
+	return 0;
 }
--- doc/exemples/server.cpp
+++ doc/exemples/server.cpp
@@ -1,88 +1,81 @@
-#include <sys/types.h> /* Voir NOTES */
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <errno.h>
 #include <string>
+#include <cstring>
 #include <iostream>
 #include <unistd.h>
-#include <arpa/inet.h>
-
+#include <cerrno>
 
 int main(int argc, char** argv)
 {
-    (void) argc;
-    (void) argv;
+	(void) argc;
+	(void) argv;
 
-    // adresse localhost
-    // const char    *ip = "127.0.0.1";
-    // port TCP de la connexion
-    int     port = 2042;
+	// adresse localhost
+	const char    *ip = "127.0.0.1";
+	// port TCP de la connexion
+	int     port = 8080;
 
-    int     server_sock, client_sock;
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
-    socklen_t addr_size;
+	int     server_sock;
+	int		client_sock;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	socklen_t addr_size;
 
-    char    buffer[1024];
-    int     n;
+	char    buffer[1024];
+	int     n;
 
-    server_sock = socket(PF_INET, SOCK_STREAM, 0);
+	server_sock = socket(PF_INET, SOCK_STREAM, 0);
 
-    if (server_sock < 0)
-    {
-        perror("[-] socket error\n");
-        exit(errno);
-    }
+	if (server_sock < 0)
+	{
+		std::cerr << "[-] socket error" << std::endl;
+		return (errno);
+	}
 
-    std::cout << "[+] TCP socket created." << std::endl;
+	std::cout << "[+] TCP socket created." << std::endl;
 
-    memset(&server_addr, '\0', sizeof(server_addr));
-    server_addr.sin_family = PF_INET;
-    server_addr.sin_port = port;
-    // server_addr.sin_addr.s_addr = inet_addr(ip);
+	memset(&server_addr, '\0', sizeof(server_addr));
+	server_addr.sin_family = PF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = inet_addr(ip);
+	
+	n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	if (n < 0)
+	{
+		std::cerr << "[-] bind error" << std::endl;	
+		return (errno);
+	}
 
-    if(inet_pton(PF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0)
-    {
-        perror("[-] Invalid address / Address not supported \n");
-        return -1;
-    }
-    
-    n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if (n < 0)
-    {
-        perror("[-] bind error\n");
-        exit(errno);
-    }
+	std::cout << "[+] bind the port number : " << port << std::endl;
 
-    std::cout << "[+] bind the port number : " << port << std::endl;
+	listen(server_sock, 5);
 
-    listen(server_sock, 5);
-
-    std::cout << "[+] listening... " << std::endl;
+	std::cout << "[+] listening... " << std::endl;
    
-    while (1)
-    {
-        addr_size = sizeof(client_addr);
-        client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
-        std::cout << "[+] client connected. " << std::endl;
+	while (1)
+	{
+		addr_size = sizeof(client_addr);
+		client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
+		std::cout << "[+] client connected. " << std::endl;
    
-        bzero(buffer, 1024);
-        recv(client_sock, buffer, sizeof(buffer), 0);
+		bzero(buffer, 1024);
+		recv(client_sock, buffer, sizeof(buffer), 0);
 
-        std::cout << "[+] client : " << buffer << std::endl;
+		std::cout << "[+] client : " << buffer << std::endl;
   
-        bzero(buffer, 1024);
-        strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
+		bzero(buffer, 1024);
+		strcpy(buffer, "HI, THIS IS SERVER. HAVE A NICE DAY!!!");
 
-        std::cout << "[+] server send :" << buffer << std::endl;
+		std::cout << "[+] server send : " << buffer << std::endl;
 
-        send(client_sock, buffer, strlen(buffer), 0);
-    
-        close(client_sock);
-        std::cout << "[+]Client disconnected." << std::endl;
+		send(client_sock, buffer, strlen(buffer), 0);
+	
+		close(client_sock);
+		std::cout << "[+] client disconnected." << std::endl;
 
-    }
+	}
 
-    return (0);
+	return (0);
 
 }
