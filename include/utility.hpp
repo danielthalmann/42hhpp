8d9331355217feba86c818242b439a0c9e765d69
Daniel Thalmann <daniel@thalmann.li>
2023-02-01 10:30:34 +0100

    add utilities

--- include/utility.hpp
+++ include/utility.hpp
@@ -2,12 +2,15 @@
 # define UTILITY_HPP
 
 # include <vector>
+# include <map>
 # include <string>
 //# include <iostream>
 #include <sstream>
 
 namespace utils {
 	std::vector<std::string> split(std::string str, std::string delimiter);
+
+	char** mapStringToArray(const std::map<std::string, std::string> &m);
 
 	template <typename T>
 	std::string numberToString(T pNumber)
--- src/utility.cpp
+++ src/utility.cpp
@@ -12,3 +12,30 @@ std::vector<std::string> utils::split(std::string str, std::string delimiter) {
 
 	return (vec);
 }
+
+char** utils::mapStringToArray(const std::map<std::string, std::string> &m)
+{
+	char **a;
+	char *s;
+	int pos;
+
+	a = new char*[m.size() + 1];
+	a[m.size()] = 0;
+
+	for (std::map<std::string, std::string>::const_iterator i = m.begin(); i != m.end(); i++)
+	{
+		int size = i->first.size() + i->second.size() + 1;
+		s = new char[size + 1];
+		s[size] = 0;
+		pos = 0;
+
+		for(int j = 0; j < i->first.size(); j++) {
+			s[pos++] = i->first[j]; 
+		}
+		s[pos++] = '=';
+		for(int j = 0; j < i->second.size(); j++) {
+			s[pos++] = i->second[j]; 
+		}
+	}
+	
+}
