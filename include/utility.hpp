eb03e68189c64b15be6f8d3065fea753bb75803c
Daniel Thalmann <daniel@thalmann.li>
2023-02-04 14:29:36 +0100

    add trim utility function

--- include/utility.hpp
+++ include/utility.hpp
@@ -8,6 +8,7 @@
 #include <sstream>
 
 namespace utils {
+
 	std::vector<std::string> split(std::string str, std::string delimiter);
 
 	char** mapStringToArray(const std::map<std::string, std::string> &m);
@@ -20,6 +21,11 @@ namespace utils {
 		oss << pNumber;
 		return oss.str();
 	}
+
+	void ltrim(std::string &s);
+	void rtrim(std::string &s);
+	std::string& trim(std::string &s);
+
 }
 
 #endif
--- src/utility.cpp
+++ src/utility.cpp
@@ -1,51 +1,78 @@
 #include "utility.hpp"
+#include <algorithm>
 
-std::vector<std::string> utils::split(std::string str, std::string delimiter) {
-	size_t pos;
-	std::vector<std::string> vec;
+namespace utils {
+		
+	std::vector<std::string> split(std::string str, std::string delimiter) {
+		size_t pos;
+		std::vector<std::string> vec;
 
-	do {
-		pos = str.find(delimiter);
-		vec.push_back(str.substr(0, pos));
-		str.erase(0, pos + delimiter.length());
-	} while (pos != std::string::npos);
+		do {
+			pos = str.find(delimiter);
+			vec.push_back(str.substr(0, pos));
+			str.erase(0, pos + delimiter.length());
+		} while (pos != std::string::npos);
 
-	return (vec);
-}
-
-char** utils::mapStringToArray(const std::map<std::string, std::string> &m)
-{
-	char **a;
-	int pos, index;
-
-	a = new char*[m.size() + 1];
-	a[m.size()] = 0;
-	index = 0;
-
-	for (std::map<std::string, std::string>::const_iterator i = m.begin(); i != m.end(); i++)
-	{
-		int size = i->first.size() + i->second.size() + 1;
-		a[index] = new char[size + 1];
-		a[index][size] = 0;
-		pos = 0;
-		for(size_t j = 0; j < i->first.size(); j++) {
-			a[index][pos++] = i->first[j]; 
-		}
-		a[index][pos++] = '=';
-		for(size_t j = 0; j < i->second.size(); j++) {
-			a[index][pos++] = i->second[j]; 
-		}
-		++index;
+		return (vec);
 	}
-	return a;
-}
 
-void utils::freeArray(char** a)
-{
-	unsigned int i = 0;
-	while (a && a[i])
+	char** mapStringToArray(const std::map<std::string, std::string> &m)
 	{
-		delete a[i++];
+		char **a;
+		int pos, index;
+
+		a = new char*[m.size() + 1];
+		a[m.size()] = 0;
+		index = 0;
+
+		for (std::map<std::string, std::string>::const_iterator i = m.begin(); i != m.end(); i++)
+		{
+			int size = i->first.size() + i->second.size() + 1;
+			a[index] = new char[size + 1];
+			a[index][size] = 0;
+			pos = 0;
+			for(size_t j = 0; j < i->first.size(); j++) {
+				a[index][pos++] = i->first[j]; 
+			}
+			a[index][pos++] = '=';
+			for(size_t j = 0; j < i->second.size(); j++) {
+				a[index][pos++] = i->second[j]; 
+			}
+			++index;
+		}
+		return a;
 	}
-	delete a;	
+
+	void freeArray(char** a)
+	{
+		unsigned int i = 0;
+		while (a && a[i])
+		{
+			delete a[i++];
+		}
+		delete a;	
+	}
+
+
+	bool isNotSpace(unsigned char ch) {
+			return !std::isspace(ch);
+	}
+
+	// trim from start (in place)
+	void ltrim(std::string &s) {
+		s.erase(s.begin(), std::find_if(s.begin(), s.end(), isNotSpace));
+	}
+
+	// trim from end (in place)
+	void rtrim(std::string &s) {
+		s.erase(std::find_if(s.rbegin(), s.rend(), isNotSpace).base(), s.end());
+	}
+
+	// trim from both ends (in place)
+	std::string& trim(std::string &s) {
+		rtrim(s);
+		ltrim(s);
+		return s;
+	}
+
 }
