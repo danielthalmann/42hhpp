4a619afdd867ab0319a36ea77ebfca63e8932d9b
Daniel Thalmann <daniel@thalmann.li>
2023-02-07 13:18:36 +0100

    test web site

---
+++ test/www/domain1/cgi/result.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>HTML Result Page for Testing</title>
+</head>
+
+<body>
+
+<h1>Testing result</h1>
+
+content : <?php echo _Form["content"] ?>
+
+</body>
+
+</html>
