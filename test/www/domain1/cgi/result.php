94b040cace49a11019aeab540d84e1778282d52d
Daniel Thalmann <dthalman@c1r5s8.42lausanne.ch>
2023-02-15 14:59:32 +0100

    add cgi bin

--- test/www/domain1/cgi/result.php
+++ test/www/domain1/cgi/result.php
@@ -9,8 +9,18 @@
 
 <h1>Testing result</h1>
 
-content : <?php echo _Form["content"] ?>
+content : <?php echo phpinfo() ?>
 
 </body>
 
 </html>
+
+
+
+
+
+export GATEWAY_INTERFACE="CGI/1.1"
+export REDIRECT_STATUS=200
+export SERVER_PROTOCOL="HTTP/1.1"
+export DOCUMENT_ROOT="/Users/dthalman/projets/42hhpp/test/www/domain1/cgi/"
+export SCRIPT_NAME="/Users/dthalman/projets/42hhpp/test/www/domain1/cgi/result.php"
