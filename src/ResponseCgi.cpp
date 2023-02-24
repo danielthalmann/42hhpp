0ff2bbac1807ffdc89ed66e3e86a447cdf7a0544
Daniel Thalmann <daniel@thalmann.li>
2023-02-24 23:57:32 +0100

    it not need duplicate the raw function in ResponseCGI

--- src/ResponseCgi.cpp
+++ src/ResponseCgi.cpp
@@ -15,42 +15,6 @@ namespace hhpp {
 
 	}
 
-	std::string ResponseCgi::raw()
-	{
-
-		std::string statusMessage;
-		int status = _status;
-
-		try {
-			statusMessage = _totalStatus.at(status);
-		} catch (std::exception &e) {
-			status = 200;
-			statusMessage = "OK";
-		}
-
-		setBody(_cgi->execute(_script, *_request));
-
-		prepareResponse();
-
-		status = 200;
-		statusMessage = "OK";
-		setStatus(status);
-
-		std::string dataSend;
-
-		dataSend.append("HTTP/1.1");
-		dataSend.append(" ");
-		dataSend.append(utils::numberToString(status));
-		dataSend.append(" ");
-		dataSend.append(statusMessage);
-		dataSend.append("\n");
-		dataSend.append(_header.raw());
-		dataSend.append("\n");
-		dataSend.append(_body);
-
-		return dataSend;
-	}
-
 	void ResponseCgi::prepareResponse() {
 		std::string header;
 		std::vector<std::string> token;
